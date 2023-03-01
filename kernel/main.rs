ae4d85a03531fb28fee0e51c3a948c7f8ef50d64
Dhanush <dhanush18245@gmail.com>
2023-03-01 22:33:54 +0530

    cleaned main.rs

--- kernel/main.rs
+++ kernel/main.rs
@@ -156,7 +156,6 @@ pub fn boot_kernel(#[cfg_attr(debug_assertions, allow(unused))] bootinfo: &BootI
 
     // Initialize memory allocators first.
     interrupt::init();
-    profiler.lap_time("global interrupt init");
 
     #[cfg(test)]
     {
@@ -166,24 +165,16 @@ pub fn boot_kernel(#[cfg_attr(debug_assertions, allow(unused))] bootinfo: &BootI
 
     // Initialize kernel subsystems.
     pipe::init();
-    profiler.lap_time("pipe init");
     poll::init();
-    profiler.lap_time("poll init");
     procfs::init();
-    profiler.lap_time("procfs init");
     devfs::init();
-    profiler.lap_time("devfs init");
     tmpfs::init();
-    profiler.lap_time("tmpfs init");
     initramfs::init();
-    profiler.lap_time("initramfs init");
     unikernel_api::kernel_ops::init(&ApiOps);
-    profiler.lap_time("unikernel_api init");
 
     // Load kernel extensions.
-    info!("kext: Loading virtio_net...");
+    info!("Loading virtio_net...");
     virtio_net::init();
-    profiler.lap_time("virtio_net init");
 
     // Initialize device drivers.
     unikernel_api::kernel_ops::init_drivers(
@@ -191,11 +182,9 @@ pub fn boot_kernel(#[cfg_attr(debug_assertions, allow(unused))] bootinfo: &BootI
         &bootinfo.pci_allowlist,
         &bootinfo.virtio_mmio_devices,
     );
-    profiler.lap_time("drivers init");
 
     // Connect to the network.
     net::init_and_start_dhcp_discover(bootinfo);
-    profiler.lap_time("net init");
 
     // Prepare the root file system.
     let mut root_fs = RootFs::new(INITRAM_FS.clone()).unwrap();
@@ -218,57 +207,14 @@ pub fn boot_kernel(#[cfg_attr(debug_assertions, allow(unused))] bootinfo: &BootI
         .mount(tmp_dir, TMP_FS.clone())
         .expect("failed to mount tmpfs");
 
-    // Open /dev/console for the init process.
     let console = root_fs
         .lookup_path(Path::new("/dev/console"), true)
         .expect("failed to open /dev/console");
 
-    // let argv0 = "/etc/init";
-
-    // Open the init's executable.
-    // let argv0 = if option_env!("INIT_SCRIPT").is_some() {
-    //     "/bin/sh"
-    // } else {
-    //     "/sbin/init"
-    // };
-    // let executable_path = root_fs
-    //    .lookup_path(Path::new(argv0), true)
-    //    .expect("failed to open the init executable");
-
     // We cannot initialize the process subsystem until INITIAL_ROOT_FS is initialized.
     INITIAL_ROOT_FS.init(|| Arc::new(SpinLock::new(root_fs)));
 
-    profiler.lap_time("root fs init");
-
     process::init();
-    profiler.lap_time("process init");
-
-    // Process::new_init_process(
-    //    INITIAL_ROOT_FS.clone(),
-    //    executable_path,
-    //    console,
-    //    &[b"/etc/init"],
-    // )
-    // .expect("failed to execute");
-
-    // Create the init process.
-    // if let Some(script) = option_env!("INIT_SCRIPT") {
-    //     let argv = &[b"sh", b"-c", script.as_bytes()];
-    //     info!("running init script: {:?}", script);
-    //     Process::new_init_process(INITIAL_ROOT_FS.clone(), executable_path, console, argv)
-    //         .expect("failed to execute the init script: ");
-    // } else {
-    //     info!("running /sbin/init");
-    //     Process::new_init_process(
-    //         INITIAL_ROOT_FS.clone(),
-    //         executable_path,
-    //         console,
-    //         &[b"/sbin/init"],
-    //     )
-    //     .expect("failed to execute /sbin/init");
-    // }
-
-    profiler.lap_time("first process init");
 
     // We've done the kernel initialization. Switch into the init...
     switch();
